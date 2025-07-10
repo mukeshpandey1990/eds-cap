dedf64b00efd5b1340a15a7d836dec94cdc02ee6
Mukesh <mukeshp@jobcloudinc.com>
2025-07-10 15:06:36 +0530

    style

---
+++ blocks/heroimage/heroimage.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("bajaj-text");
+  const title = "Bajaj Technology Services";
+  const description =
+    "is a part of Bajaj Group conglomerate having a 100-year legacy of innovation and excellence with > $100 Bn market capitalization";
+
+  container.innerHTML = `<h2>${title}</h2><p>${description}</p>`;
+});
